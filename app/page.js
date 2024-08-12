bb94af817f249fcf06d68f59d47a45679f74044b
Roy <157291828+Rigriz@users.noreply.github.com>
2024-08-12 15:44:01 +0530

    major update on main page on dynamic content
    Completed main.js page

--- app/page.js
+++ app/page.js
@@ -1,33 +1,15 @@
-import { MongoClient } from "mongodb";
-import "dotenv/config";
-const connectToMongo = async () => {
-  const uri = "mongodb+srv://5g:5g@cluster0.o98aylc.mongodb.net/";
-  try {
-    const client = new MongoClient(uri);
-    await client.connect();
-    console.log("Connected successfully to MongoDB");
-    const db = client.db("Isrp");
-    const cursor = db.collection("webpages").find();
-    const documents = await cursor.toArray();
-    const jsonData = documents.map((doc) => JSON.stringify(doc));
-    console.log(jsonData);
-    await client.close();
-    return jsonData;
-  } catch (err) {
-    console.error("error connecting to MongoDB", err);
-  }
-};
-const data = connectToMongo();
-function Home() {
+import styles from "./Styles/page.module.css";
+import style from "./Styles/home.module.css";
+import { MainPage } from "./pages/main";
+import Link from "next/link";
+
+export default async function Home() {
   return (
-    <div>
-      <div>
-        <h1>Hii</h1>
-        <p>hi</p>
-        <p>{data}</p>
+    <main className={styles.main}>
+      <div className={style.imagecontainer}>
+        <img className={style.image} src="./needed/main.png" alt="College Picture" />
       </div>
-    </div>
+      <MainPage />
+    </main>
   );
 }
-
-export default Home;
---
+++ app/pages/main.js
@@ -0,0 +1,182 @@
+import DOMPurify from "isomorphic-dompurify";
+import getStaticProps from "./getdata";
+import styles from "../Styles/main.module.css";
+import Link from "next/link";
+async function featch() {
+  try {
+    const data = await getStaticProps();
+    return data.props.webContent.Home; //Return the homepage content on success
+  } catch (error) {
+    console.error(
+      "Error retrieving data from MongoDB getDataFromMongo:",
+      error,
+    );
+    return { error: error.message }; // Return an error object on failure
+  }
+}
+async function MainPage() {
+  const data = await featch();
+  console.log(data.page,"gotta");
+  //console.log(data, "heimana");
+  return (
+    <>
+      <div className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.leftContainer}>
+            <div className={styles.welcome}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(data.title),
+                }}
+              />
+            </div>
+            <div className={styles.content}>
+              <p
+                className={styles.p}
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(data.description),
+                }}
+              />
+              <p
+                className={styles.p}
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(data.description2),
+                }}
+              />
+            </div>
+            <div className={styles.board}>
+              <div className={styles.recognition}>
+                <p
+                  className={styles.p}
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(data.Recognition),
+                  }}
+                />
+              </div>
+              <div className={styles.dte}>
+                <p
+                  className={styles.p}
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(data.SubRecognition),
+                  }}
+                />
+              </div>
+              <div className={styles.grievance}>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(data.GrievanceTitle),
+                  }}
+                />{" "}
+                <a href={data.GrievanceLink}>CLICK HERE</a>
+              </div>
+              <div className={styles.disclosure}>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(data.MandatoryTitle),
+                  }}
+                />{" "}
+                <a href={data.MandatoryLink}>CLICK HERE</a>
+              </div>
+            </div>
+          </div>
+          <div className={styles.rightContainer}>
+            <div className={styles.mission}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(data.MissionTitle),
+                }}
+              />
+            </div>
+            <div className={styles.missionText}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(data.MissionDescription),
+                }}
+              />
+            </div>
+            <div className={styles.vision}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(data.VisionTitle),
+                }}
+              />
+            </div>
+            <div className={styles.visionText}>
+              <p
+                dangerouslySetInnerHTML={{
+                  __html: DOMPurify.sanitize(data.VisionDescription),
+                }}
+              />
+            </div>
+            <div className={styles.offers}>
+              <div className={styles.offersTitle}>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: DOMPurify.sanitize(data.CoursesTitle),
+                  }}
+                />
+              </div>
+              <div className={styles.coursesList}>
+                <Link
+                  key={"course1"}
+                  href={"http://www.google.com"}
+                  className={styles.courseLink}
+                >
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(data.CoursesOne),
+                    }}
+                  />
+                </Link>
+                <Link
+                  key={"course2"}
+                  href={"http://www.google.com"}
+                  className={styles.courseLink}
+                >
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(data.CoursesTwo),
+                    }}
+                  />
+                </Link>
+                <Link
+                  key={"course3"}
+                  href={"http://www.google.com"}
+                  className={styles.courseLink}
+                >
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(data.CoursesThree),
+                    }}
+                  />
+                </Link>
+                <Link
+                  key={"course4"}
+                  href={"http://www.google.com"}
+                  className={styles.courseLink}
+                >
+                  <p
+                    dangerouslySetInnerHTML={{
+                      __html: DOMPurify.sanitize(data.CorusesFoure),
+                    }}
+                  />
+                </Link>
+              </div>
+              <div>
+                <img
+                  className={styles.image}
+                  src="./needed/ragging-free-logo.png"
+                  alt="Ragging-logo"
+                />
+                <span className={styles.Ragginglink}>
+                  For Anti-Ragging Committee list click here
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export { MainPage };
