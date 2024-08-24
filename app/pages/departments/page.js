0c71adf9a948ab7462e4deb407e23a5494f7f08c
Rigriz <samsammith9@gmail.com>
2024-08-24 18:48:43 +0530

    department invalved

--- app/pages/departments/page.js
+++ app/pages/departments/page.js
@@ -1,14 +1,15 @@
 import styles from "@/app/Styles/page.module.css";
 import style from "@/app/Styles/home.module.css";
 import Link from "next/link";
+import CSE from "./Cse/cse.js"
 
-export default async function Home() {
+export default async function department() {
     return (
         <main className={styles.main}>
             <div className={style.imagecontainer}>
                 <img className={style.image} src="./needed/main.png" alt="College Picture" />
             </div>
-            
+            <CSE />
         </main>
     );
 }
