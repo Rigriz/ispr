13fe84e70dbf74fbc45e0a088f789889b866859b
Rigriz <samsammith9@gmail.com>
2024-08-24 17:50:15 +0530

    navlink .js added

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,7 @@
 import { Inter } from "next/font/google";
 import "./Styles/globals.css";
 import styles from "@/app/Styles/main.module.css";
-<<<<<<< HEAD
 import Navbar from "./Components/NavBar.js";
-=======
-import Navbar from "./components/NavBar";
->>>>>>> a1e1fcdd6300edb8ebaee823f03a0ba1e7e8f819
 
 const inter = Inter({ subsets: ["latin"] });
 
