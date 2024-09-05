14b4011bfd67185987ca06dac7b884c90cbd8279
Roy <157291828+Rigriz@users.noreply.github.com>
2024-09-05 13:38:37 +0530

    main home title

--- app/(pages)/page.js
+++ app/(pages)/page.js
@@ -2,10 +2,13 @@ import styles from "../Styles/page.module.css";
 import style from "../Styles/home.module.css";
 import { MainPage } from "./main/main.js";
 
+
+export const metadata ={
+  title:"ISRP"
+}
 export default async function Home() {
   return (
     <>
-     
       <main className={styles.main}>
         <div className={style.imagecontainer}>
           <img className={style.image} src="./needed/main.png" alt="College Picture" />
