80175205569e38772670293a1efdbd46bab917d3
Roy <157291828+Rigriz@users.noreply.github.com>
2024-09-09 07:58:39 +0530

    fetchdata update

--- app/pages/api/fetchdata.js
+++ app/pages/api/fetchdata.js
@@ -463,7 +463,7 @@ export default async function getStaticProps(request) {
             props: {
                 webContent: record, //sending the { json data }
             },
-            revalidate: 600000, // revalidate every 400 second
+            revalidate: 2000, // revalidate every 400 second
         };
     } catch (err) {
         console.error("Error fetching data from MongoDB", err);
