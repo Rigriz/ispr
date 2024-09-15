4ac1d99fc3bf176b25bbdfcdc2f9add72cfd9fbf
Roydevo <157291828+Rigriz@users.noreply.github.com>
2024-09-15 20:42:03 +0530

    api data initated

--- app/(pages)/api/fetchdata.js
+++ app/(pages)/api/fetchdata.js
@@ -1,16 +1,16 @@
-import { MongoClient } from 'mongodb';
+//import { MongoClient } from 'mongodb';
 
 export default async function getStaticProps(request) {
   try {
-    const client = await MongoClient.connect(process.env.MONGODB_URI); 
-    const db = client.db(process.env.DB_NAME);
-    const collection = db.collection(process.env.WEB_CONTENT);
+   // const client = await MongoClient.connect(process.env.MONGODB_URI); 
+   // const db = client.db(process.env.DB_NAME);
+   // const collection = db.collection(process.env.WEB_CONTENT);
     // Project only necessary fields (excluding _id)
-    const cursor = collection.find({}, { projection: { _id: 0 } });
-   const documents = await cursor.toArray();
-   await client.close();
+   // const cursor = collection.find({}, { projection: { _id: 0 } });
+ //  const documents = await cursor.toArray();
+ //  await client.close();
     // Transform data into an object with page as the key
-/*
+
   const documents = [{
       "_id": {
         "$oid": "66a72a45305c1474d56b4288"
@@ -463,7 +463,6 @@ export default async function getStaticProps(request) {
       "Footer1": "CLICK HERE  FOR ONLINE GRS PROCEDURE",
     }
     ] 
-*/
     const json = documents.reduce((acc, item) => {
       if (item.page) { // Ensure the field exists for a unique key
         acc[item.page] = item;
---
+++ app/api/webcontent/route.js
@@ -0,0 +1,66 @@
+import { MongoClient, ServerApiVersion } from 'mongodb';
+import { NextResponse } from 'next/server';
+//import fetchData from "@/app/(pages)/api/fetchdata";
+export async function getStaticProps(request) {
+    const uri = process.env.MONGODB_URI;
+    //console.log(request);
+    const client = new MongoClient(uri, {
+        serverApi: {
+            version: ServerApiVersion.v1,
+            strict: true,
+            deprecationErrors: true,
+        }
+    });
+    try {
+        // Connect to the MongoDB cluster
+        await client.connect();
+        // Access the database and collection
+        const database = client.db(process.env.DB_NAME);
+        const collection = database.collection(process.env.WEB_CONTENT);
+        if (!collection) {
+            throw new Error(`Collection not found: ${process.env.WEB_CONTENT}`);
+        }
+        // Perform the query
+        const result = await collection.find({}, { projection: { _id: 0 } }).toArray();
+        await client.close();
+        // Process the result
+        const json = result.reduce((acc, item) => {
+            if (item.page) {
+                // Ensure the field exists for a unique key
+                acc[item.page] = item;
+            }
+            return acc;
+        }, {});
+        return json;
+        //console.log(json); whole data of webcontent
+        //const record = json[request];
+        //console.log(record,'sdf')
+        //return record;
+    } catch (error) {
+        if (error.codeName === 'AtlasError') {
+            console.error('AtlasError:', error.code, error.codeName, error.errmsg);
+            return { error: 'An error occurred while connecting to the database. Please try again later.' };
+        } else {
+            console.error(error);
+            return { error: error.message };
+        }
+    } finally {
+        // Close the client connection
+        await client.close();
+    }
+}
+export async function GET(request) {
+    const url = new URL(request.url);
+    const queryParams = Object.fromEntries(url.searchParams.entries());
+    const webcontent = queryParams.webcontent;
+    //console.log(webcontent);
+    const data = await getStaticProps(webcontent);
+    // console.log(webcontent);
+    // const webcontent= request.query.webcontent;
+    //const data = await getStaticProps(webcontent);
+    // const data = await fetchData(webcontent);
+    console.log(data);
+    //const res = await request.json()
+    //const data = await fetchdata();
+    return NextResponse.json(data);
+}
